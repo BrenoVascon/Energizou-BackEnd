e1e2a3e28aad08f827061088b9e206cebb52e26a
Breno Vasc <beniolimavasc@gmail.com>
2023-11-03 02:14:54 -0300

    fix: routes

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -5,7 +5,7 @@ import IUser from '../interfaces/IUser';
 
 const userRouter = Router();
 
-userRouter.post('/users/', async (_req: Request, res: Response): Promise<Response> => { 
+userRouter.post('/', async (_req: Request, res: Response): Promise<Response> => { 
     try { 
       const newUser = await UserRepository.createNewUser(_req.body); 
       return res.status(201).json(newUser);
@@ -16,12 +16,12 @@ userRouter.post('/users/', async (_req: Request, res: Response): Promise<Respons
   });
   
 
-userRouter.get('/users/', async (_req: Request, res: Response): Promise<Response> => {
+userRouter.get('/', async (_req: Request, res: Response): Promise<Response> => {
     const users = await UserRepository.getUsers();
     return res.status(200).json(users);
 });
 
-userRouter.get('/users/:cnpj', async (req: Request, res: Response): Promise<Response> => {
+userRouter.get('/:cnpj', async (req: Request, res: Response): Promise<Response> => {
     const cnpj = req.params.CNPJ;
     const user = await UserRepository.getUsersByCnpj(cnpj);
 
@@ -32,7 +32,7 @@ userRouter.get('/users/:cnpj', async (req: Request, res: Response): Promise<Resp
     }
 });
 
-userRouter.put('/users/:cnpj', async (req: Request, res: Response): Promise<Response> => {
+userRouter.put('/:cnpj', async (req: Request, res: Response): Promise<Response> => {
     const cnpj = req.params.CNPJ;
     const userData = req.body;
 
@@ -52,7 +52,7 @@ userRouter.put('/users/:cnpj', async (req: Request, res: Response): Promise<Resp
 });
 
 
-userRouter.delete('/users/:cnpj', async (req: Request, res: Response): Promise<Response> => {
+userRouter.delete('/:cnpj', async (req: Request, res: Response): Promise<Response> => {
     const cnpj = req.params.CNPJ;
     const deleted = await UserRepository.deleteByCNPJ(cnpj)
 
