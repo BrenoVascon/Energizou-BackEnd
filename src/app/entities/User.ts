a784b10b17feb745613511133cc20498f2584604
Breno Vasc <beniolimavasc@gmail.com>
2023-11-22 14:49:07 -0300

    Alterações

--- src/app/entities/User.ts
+++ src/app/entities/User.ts
@@ -3,7 +3,7 @@ import { IsString, Matches, IsNotEmpty } from 'class-validator';
 
 import validator from 'validator';
 
-@Entity('Users')
+@Entity('users')
 class User {
   @PrimaryGeneratedColumn()
   id: number;
--- src/database/migrations/1699338159409-CreateUsersTable.ts
+++ src/database/migrations/1699338159409-CreateUsersTable.ts
@@ -3,74 +3,8 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm"
 export class CreateUsersTable1699338159409 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-          new Table({
-            name: 'users',
-            columns: [
-              {
-                name: 'id',
-                type: 'int',
-                isPrimary: true,
-                generationStrategy: 'increment',
-              },
-              {
-                name: 'name',
-                type: 'varchar',
-                length: '100',
-                isNullable: false,
-              },
-              {
-                name: 'password',
-                type: 'varchar',
-                length: '100',
-                isNullable: false,
-              },
-              {
-                name: 'company',
-                type: 'varchar',
-                length: '100',
-                isNullable: false,
-              },
-              {
-                name: 'cnpj',
-                type: 'varchar',
-                length: '20', 
-                isNullable: false,
-              },
-              {
-                name: 'cep',
-                type: 'varchar',
-                length: '10', 
-                isNullable: false,
-              },
-              {
-                name: 'address',
-                type: 'varchar',
-                length: '100',
-                isNullable: false,
-              },
-              {
-                name: 'number',
-                type: 'varchar',
-                length: '4', 
-                isNullable: false,
-              },
-              {
-                name: 'phone',
-                type: 'varchar',
-                length: '25', 
-                isNullable: false,
-              },
-              {
-                name: 'email',
-                type: 'varchar',
-                length: '100',
-                isNullable: false,
-              },
-            ],
-          })
-        );
-      }
+        
+          }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.dropTable('users')
